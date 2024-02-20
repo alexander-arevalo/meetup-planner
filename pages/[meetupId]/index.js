6a835639e5de81afdb8ee744d25a6c3f2ba0f15d
zerootwooooo <zanderarevalo20@gmail.com>
2024-02-21 01:34:07 +0800

    commit

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,10 +1,13 @@
-import React, { Fragment } from "react";
+import MeetupDetail from "@/components/meetups/MeetupDetail";
 
 const MeetupDetails = () => {
   return (
-    <Fragment>
-      <img src="" alt="" />
-    </Fragment>
+    <MeetupDetail
+      image="https://imgs.search.brave.com/1Any_NYLTNsW1U_AHvrk3xM2eifmK3ky45oC7BC9W50/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9jZG4u/cGl4YWJheS5jb20v/cGhvdG8vMjAxNi8w/My8wNC8xOS8zNi9i/ZWFjaC0xMjM2NTgx/XzY0MC5qcGc"
+      title="First Title"
+      address="Address"
+      description="Describing"
+    />
   );
 };
 
